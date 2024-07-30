56d2f4cedd0dd8930b5599f97cb85ccd32c3a0ba
Dptandel <dharmin28052002@gmail.com>
2024-07-30 20:05:38 +0530

    Updated Successfully

---
+++ Kotlin_Assignments/Module_1_Introduction/Introduction.kt
@@ -0,0 +1,71 @@
+package KotlinAssignment.Module_1_Introduction
+
+/*
+*
+* 1. What is software.
+* Ans. Software is a set of instructions, programs, or data that tells a computer or other device what to do and how to do it.
+*   Software makes it easier to interact with devices and allows users to access new features and capabilities. For example,
+*   software allows users to create graphics, edit videos, and play games without needing to know how the computer's hardware works.
+*   Software is a set of instructions, data or programs used to operate computers and execute specific tasks. It is the opposite of
+*   hardware, which describes the physical aspects of a computer.
+*
+* 2. Types of application.
+* Ans. Applications can be categorized into several types based on their purpose and functionality.
+* Here are some common types:
+*
+* Web Applications: e.g., Google Docs, Facebook.
+*
+* Mobile Applications: e.g., Instagram, Uber.
+*
+* Desktop Applications: e.g., Microsoft Office, Adobe Photoshop.
+*
+* Enterprise Applications: e.g., Salesforce, SAP.
+*
+* Gaming Applications: e.g., Fortnite, Candy Crush.
+*
+* Utility Applications: e.g., antivirus software, file management tools.
+*
+* Educational Applications: e.g., Duolingo, Khan Academy.
+*
+* Social Media Applications: e.g., Twitter, LinkedIn.
+*
+* E-commerce Applications: e.g., Amazon, eBay.
+*
+* Streaming Applications: e.g., Spotify, Netflix.
+*
+* Each type serves distinct user needs and functions in different environments.
+*
+* 3. What is mobile application.
+* Ans. A mobile application, or mobile app, is a software application specifically designed to run on mobile devices
+* such as smartphones and tablets. These apps can be downloaded and installed from app stores (like the Apple App Store
+* or Google Play Store) and are optimized for touch interfaces.
+
+* 4. Difference between mobile application and web application.
+* Ans.
+* Criterion                     	Web apps	                               Mobile apps
+
+* Functionality	        Support simple functionality	           Support complex functionality
+
+* Cost of development	Are usually lower in comparison to         Are usually higher in comparison with web apps
+*                       mobile apps due to the common code base	   due to platform-specific requirements
+*
+* Time-to-market	    Are faster to develop	                   The development may take a longer time especially if
+*                                                                  you want to launch your app on different platforms
+*
+* Updates	            Don’t involve downloading	                Users need to download an updated version
+
+* Downloading	        Don’t need.	                                Need downloading
+
+* Monetization       	Are complicated	                            Strategies are still evolving
+
+* Ease of use	        Usually perform slower than mobile apps	    Can work offline
+
+* Discoverability	    Less discoverable as there is no market     Less discoverable as there is no marketplace for
+*                       place for web apps.	                        web apps Can be found by searching for relevant keywords
+*                                                                   on app marketplaces
+* 4. Who developed Android ?
+* Ans. Android was developed by Android Inc., which was founded in October 2003 by Andy Rubin, Rich Miner, Nick Sears,
+* and Chris White. In 2005, Google acquired Android Inc., and since then, Android has been developed and maintained by
+* Google as an open-source operating system for mobile devices.
+*
+*  */
